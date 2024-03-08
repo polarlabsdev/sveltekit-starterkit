8938a6571a86f053038c11d67c550b74a9721fcf
Jerry Zhang <jerryzhang.dev@gmail.com>
2024-03-07 21:22:25 -0500

    dummy change

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,8 +4,6 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
-
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 		following into your command line and following the prompts:
